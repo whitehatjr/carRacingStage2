8c373fc6db0068bd53c6a0cc8de82167f66ea24d
rajeev.artha <rajeev.artha@gmail.com>
2019-09-06 17:48:27 +0530

    add  tracks

--- js/Game.js
+++ js/Game.js
@@ -30,25 +30,32 @@ class Game {
     }
 
     car1 = createSprite(100,200);
+    car1.addImage("car1",car1_img);
     car2 = createSprite(300,200);
+    car2.addImage("car2",car2_img);
     car3 = createSprite(500,200);
+    car3.addImage("car3",car3_img);
     car4 = createSprite(700,200);
+    car4.addImage("car4",car4_img);
     cars = [car1, car2, car3, car4];
   }
 
   play(){
     form.hide();
-
+    
     Player.getPlayerInfo();
     
     if(allPlayers !== undefined){
+      background("#c68767");
+      image(track, 0,-displayHeight*4,displayWidth, displayHeight*5);
+      
       //var display_position = 100;
       
       //index of the array
       var index = 0;
 
       //x and y position of the cars
-      var x = 0;
+      var x = 175 ;
       var y;
 
       for(var plr in allPlayers){
@@ -65,7 +72,7 @@ class Game {
         if (index === player.index){
           cars[index - 1].shapeColor = "red";
           camera.position.x = displayWidth/2;
-          camera.position.y = cars[index-1].y
+          camera.position.y = cars[index-1].y;
         }
        
         //textSize(15);
@@ -79,6 +86,14 @@ class Game {
       player.update();
     }
 
+    if(player.distance > 3860){
+      gameState = 2;
+    }
+   
     drawSprites();
+  }
+
+  end(){
+    console.log("Game Ended");
   }
 }
--- sketch.js
+++ sketch.js
@@ -10,6 +10,16 @@ var form, player, game;
 
 var cars, car1, car2, car3, car4;
 
+var track, car1_img, car2_img, car3_img, car4_img;
+
+function preload(){
+  track = loadImage("../images/track.jpg");
+  car1_img = loadImage("../images/car1.png");
+  car2_img = loadImage("../images/car2.png");
+  car3_img = loadImage("../images/car3.png");
+  car4_img = loadImage("../images/car4.png");
+  ground = loadImage("../images/ground.png");
+}
 
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
@@ -27,5 +37,8 @@ function draw(){
   if(gameState === 1){
     clear();
     game.play();
+  }
+  if(gameState === 2){
+    game.end();
   }
 }
